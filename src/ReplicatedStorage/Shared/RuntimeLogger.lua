43af5c74edd87e1e1963c6cf6fc533bda8cdc297
Blue <99059036+Blue5123@users.noreply.github.com>
2024-05-09 12:09:26 -0400

    Version 1.1.0

--- src/ReplicatedStorage/Shared/RuntimeLogger.lua
+++ src/ReplicatedStorage/Shared/RuntimeLogger.lua
@@ -1,6 +1,7 @@
 local RuntimeLogger = {}
 RuntimeLogger.__index = RuntimeLogger
 
+-- Constructor
 function RuntimeLogger.new()
 	local self = setmetatable({
 		start = tick(),
@@ -8,20 +9,23 @@ function RuntimeLogger.new()
 	return self
 end
 
+-- Deconstructor
+function RuntimeLogger:destroy()
+	table.clear(self)
+	setmetatable(self, nil)
+	table.freeze(self)
+end
+
+-- Easy print and auto cleanup
 function RuntimeLogger:PrintTime(msg: string)
 	print((msg):format(("%0.2f"):format(self:getTime())))
 	self:destroy()
 end
 
+-- Gets the logger delta time
 function RuntimeLogger:getTime()
 	local elapsed = tick() - self.start
 	return tostring(elapsed)
-end
-
-function RuntimeLogger:destroy()
-	table.clear(self)
-	setmetatable(self, nil)
-	table.freeze(self)
 end
 
 return RuntimeLogger
